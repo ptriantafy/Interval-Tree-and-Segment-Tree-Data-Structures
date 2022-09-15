4e072991fe73e7212db8a4a724c6e2c733842917
ptriantafy <pantrian04@gmail.com>
2022-09-15 22:29:40 +0300

    new interval class

---
+++ interval.py
@@ -0,0 +1,11 @@
+# Python Code for Interval tree
+import random
+
+class Interval:
+	def __init__(self, minLow, maxLow, minSize, maxSize):
+		self.low = random.randint(minLow, maxLow)
+		self.high =self.low +  random.randint(minSize, maxSize)
+
+	def __str__(self):
+		return "[" + str(self.low) + "," + str(self.high) + "]"
+
--- main.py
+++ main.py
@@ -2,22 +2,33 @@ import numpy as np
 import pylab as pl
 from matplotlib import collections  as mc
 import random
+import interval
+
+intervalArray = []
+for i in range(5):
+# (minLow, maxLow, minSize, maxSize)
+    x = interval.Interval(5,40,10,15)
+    print(x)
 
 
-query_x = random.randint(1,80)
-query_interval=[[(query_x,50),(query_x,0)]]
-# 
-intervals=[]
-for i in range(40):
-    small_x=random.randint(0,80)
-    intervals.append([(small_x, i+2),(small_x+random.randint(40,60), i+2)])
+queryLow = random.randint(10,35)
+queryInterval=[queryLow, queryLow + random.randint(12,13)]
+print(queryInterval)
 
-intervalCollection = mc.LineCollection(intervals, colors="Blue", linewidths=3)
-queryCollection = mc.LineCollection(query_interval, colors = "Red", linewidth=3)
-fig, ax = pl.subplots()
-ax.add_collection(intervalCollection)
-ax.add_collection(queryCollection)
-print(query_interval)
-ax.autoscale()
-ax.margins(0.1)
-pl.show()
+# query_x = random.randint(1,80)
+# query_interval=[[(query_x,50),(query_x,0)]]
+# # 
+# intervals=[]
+# for i in range(40):
+#     small_x=random.randint(0,80)
+#     intervals.append([(small_x, i+2),(small_x+random.randint(40,60), i+2)])
+
+# intervalCollection = mc.LineCollection(intervals, colors="Blue", linewidths=3)
+# queryCollection = mc.LineCollection(query_interval, colors = "Red", linewidth=3)
+# fig, ax = pl.subplots()
+# ax.add_collection(intervalCollection)
+# ax.add_collection(queryCollection)
+# print(query_interval)
+# ax.autoscale()
+# ax.margins(0.1)
+# pl.show()
